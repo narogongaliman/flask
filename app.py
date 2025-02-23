11c0660e6196114c1df64093cf0ee1f4d12fac91
bofandra.muhammad <bofandra.muhammad@dto.kemkes.go.id>
2025-02-23 14:04:59 +0700

    add events

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mosque.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['CKEDITOR_SERVE_LOCAL'] = True
+app.config['CKEDITOR_HEIGHT'] = 300
+
+ckeditor = CKEditor(app)
 db = SQLAlchemy(app)
 
 from routes import *
--- models.py
+++ models.py
@@ -4,3 +4,8 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text, nullable=False)
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text, nullable=False)
